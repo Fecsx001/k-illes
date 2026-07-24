c6e9a1d0e867bb38eab0588c2a2564598f677256
Andras Fecso <fecsoandrasbalazs@gmail.com>
2026-07-24 15:07:17 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import bg from './assets/images/background.svg';
+import './App.css'
+
+function App() {
+    return <img id="background" src={bg} alt="Background" />
+}
+
+export default App
